ec4bffada0cd2074ae15340cde329627f1fe0b41
cbudtz <Splat1477>
2014-06-12 11:15:31 +0200

    Created validator - working on LoginGui validation and robustness

--- 51_02324_F14/final/javaMeasure/Validator.java
+++ 51_02324_F14/final/javaMeasure/Validator.java
@@ -1,12 +1,29 @@
 package javaMeasure;
 
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Validator {
 	public static boolean validateUsername(String s){
-		Pattern p = Pattern.compile("[a-zæøåú0-9.@]+");
-		Matcher m = p.matcher(s);
-		return m.matches();
+		return validate(s, "[^a-zA-Z0-9@\\.\\_\\-øæåØÆÅú]");
+	}
+	
+	public static boolean validatePassword(String s) {
+		boolean valid =  validate(s,"[^a-zA-Z0-9@\\.\\_\\-øæåØÆÅú]");	
+		return valid && s.length() >=4 && s.length() <=100;	
+	}
+	
+private static boolean validate(String s, String pattern) {
+	Pattern p = Pattern.compile(pattern);
+	Matcher m = p.matcher(s);
+	//return true for no illegal
+	return !m.matches();
+}
+	public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in);
+		String s = scanner.next();
+		System.out.println(Validator.validatePassword(s));
+		scanner.close();
 	}
 }
--- 51_02324_F14/final/javaMeasure/control/LoginController.java
+++ 51_02324_F14/final/javaMeasure/control/LoginController.java
@@ -27,16 +27,18 @@ public class LoginController implements ILoginController{
 //		User[] users = null;
 		try {
 			users = mainCtrl.getDatabaseController().getUserList();
+			String[] UserStrings = new String[users.size()];
+			for (int i = 0; i<users.size();i++){
+				UserStrings[i] = users.get(i).getUserName();
+			}
+			Gui.setUserList(UserStrings);
+			Gui.enableButtons(true);
 		} catch (DataBaseException e) {
 			System.err.println("could not load user list");
-			// TODO Auto-generated catch block
+			System.exit(-1);
 			e.printStackTrace();
 		}
-		String[] UserStrings = new String[users.size()];
-		for (int i = 0; i<users.size();i++){
-			UserStrings[i] = users.get(i).getUserName();
-		}
-		Gui.setUserList(UserStrings);
+		
 	}
 
 	@Override
