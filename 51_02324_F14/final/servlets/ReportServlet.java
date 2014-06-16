b5cef27a7cf15d4d5c573695de80ebb8ea97932d
kangarooooooo <eirikonielsen@gmail.com>
2014-06-16 13:23:31 +0200

    Damnit Rúni... Report virker, nu skal ting kun skrives ind i csv filen på en fornuftig måde.

--- 51_02324_F14/final/servlets/ReportServlet.java
+++ 51_02324_F14/final/servlets/ReportServlet.java
@@ -27,15 +27,14 @@ public class ReportServlet extends HttpServlet {
 	 * Default constructor. 
 	 */
 	public ReportServlet() {
-		// TODO Auto-generated constructor stub
 	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		download(request, response);
-		//processReq(request, response);
+		//download(request, response);
+		processReq(request, response);
 	}
 
 	/**
@@ -47,11 +46,17 @@ public class ReportServlet extends HttpServlet {
 
 	private void processReq(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
 		//Login check
-		if (request.getSession().getAttribute("user")==null){
-			response.sendRedirect("LoginServlet");
-		} else {
+		if (request.getSession().getAttribute("user")!=null){
+			if(request.getParameter("saveAsExcel")!=null){
+				download(request, response);
+			}
+			else if((request.getParameter("newReport")!=null)){
+				request.getRequestDispatcher("MenuServlet").forward(request, response);
+			}
+			else{
 			//Load batch from database
 			String batchName = request.getParameter("selectedbatch");
+			System.out.println(batchName);
 			DataBaseController dbctrl = (DataBaseController) request.getSession().getAttribute("database");
 			Batch batch = null;
 			BatchProfile profile = null;
@@ -63,6 +68,10 @@ public class ReportServlet extends HttpServlet {
 			} catch (DataBaseException e) {
 				e.printStackTrace();
 			}
+			}
+
+		}else {	
+			response.sendRedirect("LoginServlet");
 		}
 
 		}
@@ -71,9 +80,6 @@ public class ReportServlet extends HttpServlet {
 	 		response.setContentType("application/octet-stream");
 	 		response.setHeader("Content-Disposition",
 	 		"attachment;filename=downloadfilename.csv");
-		Writer writer = null;
-		ServletContext context = request.getSession().getServletContext();
-	 	String realContextPath = context.getRealPath(request.getContextPath());
 	 	try{
 	 		ServletOutputStream out = response.getOutputStream();
 	 		StringBuffer sb = generateCsvFileBuffer();
@@ -94,25 +100,7 @@ public class ReportServlet extends HttpServlet {
 			e.printStackTrace();
 		}finally{
 
-	 	}
-//	 	File dir = new File(realContextPath);
-//	 	dir = dir.getParentFile();
-//	 	try {
-
-//	 	FileOutputStream esc =  new FileOutputStream(new File(dir,"newfile.csv"));
-//	 	OutputStreamWriter pen = new OutputStreamWriter(esc, "utf-8");
-//	     writer = new BufferedWriter(pen);
-//	     request.getRequestDispatcher("newfile.csv").forward(request, response);
-//	 	} catch (IOException ex) {
-//	 		ex.printStackTrace();
-//	 		System.out.println("I can't let you do that, "+request.getSession().getAttribute("username")+".");
-//	   // report
-//	 	} finally {
-//	 		 
-//	    try {writer.close();
-//	    } catch (Exception ex) {}
-//	 	}
-		
+	 	}		
 	}
 	
 	private StringBuffer generateCsvFileBuffer() {
@@ -131,7 +119,6 @@ public class ReportServlet extends HttpServlet {
 		for(int i = 0; i < 7; i++){
 			reportData[i+1] = profile.getProfileSettings().get(i).getValue();
 		}
-		//		reportData[0] = profile.getProfileSettings().get(0).getValue();
 		int reportIndex = 8;
 		int row = 0;
 		for(int i = 7; i < 16; i++){
@@ -181,53 +168,6 @@ public class ReportServlet extends HttpServlet {
 			System.out.print(i + ": " + reportData[i] + "\t");
 			}
 		}
-		//		// length row	
-		//		
-		//		reportData[1] = profile.getProfileSettings().get(7).getValue();
-		//		reportData[2] = String.valueOf(Double.parseDouble(profile.getProfileSettings().get(7).getValue()) - Double.parseDouble(profile.getProfileSettings().get(8).getValue()));
-		//		reportData[3] = String.valueOf(Double.parseDouble(profile.getProfileSettings().get(7).getValue()) + Double.parseDouble(profile.getProfileSettings().get(8).getValue()));
-		//		reportData[4] = profile.getProfileSettings().get(9).getValue();
-		//		reportData[5] = null; // do not have the needed info
-		//		reportData[6] = null; // do not have the needed info
-		//		reportData[7] = null; // do not have the needed info
-		//		
-		//		// width row
-		//		reportData[8] = profile.getProfileSettings().get(10).getValue();
-		//		reportData[9] = String.valueOf(Double.parseDouble(profile.getProfileSettings().get(10).getValue()) - Double.parseDouble(profile.getProfileSettings().get(11).getValue()));
-		//		reportData[10] = String.valueOf(Double.parseDouble(profile.getProfileSettings().get(10).getValue()) + Double.parseDouble(profile.getProfileSettings().get(11).getValue()));
-		//		reportData[11] = profile.getProfileSettings().get(12).getValue();
-		//		reportData[12] = null;
-		//		reportData[13] = null;
-		//		reportData[14] = null;
-		//		
-		//		// thickness row
-		//		reportData[15] = profile.getProfileSettings().get(13).getValue();
-		//		reportData[16] = String.valueOf(Double.parseDouble(profile.getProfileSettings().get(13).getValue()) - Double.parseDouble(profile.getProfileSettings().get(14).getValue()));
-		//		reportData[17] = String.valueOf(Double.parseDouble(profile.getProfileSettings().get(13).getValue()) + Double.parseDouble(profile.getProfileSettings().get(14).getValue()));
-		//		reportData[18] = profile.getProfileSettings().get(15).getValue();
-		//		reportData[19] = null;
-		//		reportData[20] = null;
-		//		reportData[21] = null;
-		//		
-		//		// capacitans row
-		//		reportData[22] = profile.getProfileSettings().get(26).getValue();
-		//		reportData[23] = String.valueOf(Double.parseDouble(profile.getProfileSettings().get(26).getValue()) - Double.parseDouble(profile.getProfileSettings().get(27).getValue()));
-		//		reportData[24] = String.valueOf(Double.parseDouble(profile.getProfileSettings().get(26).getValue()) + Double.parseDouble(profile.getProfileSettings().get(27).getValue()));
-		//		reportData[25] = profile.getProfileSettings().get(28).getValue();
-		//		reportData[26] = null;
-		//		reportData[27] = null;
-		//		reportData[28] = null;
-		//		
-		//		// stroke row
-		//		reportData[29] = profile.getProfileSettings().get(29).getValue();
-		//		reportData[30] = String.valueOf(Double.parseDouble(profile.getProfileSettings().get(29).getValue()) - Double.parseDouble(profile.getProfileSettings().get(30).getValue()));
-		//		reportData[31] = String.valueOf(Double.parseDouble(profile.getProfileSettings().get(29).getValue()) + Double.parseDouble(profile.getProfileSettings().get(30).getValue()));
-		//		reportData[32] = profile.getProfileSettings().get(31).getValue();
-		//		reportData[33] = null;
-		//		reportData[34] = null;
-		//		reportData[35] = null;
-
-
 		return reportData;
 	}
 
